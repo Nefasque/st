dd592625b8dd1ec1f7016547c0572d3d0a7b9bff
Nefasque <Pedro0fici4lDev@gmail.com>
2024-01-11 15:58:34 -0400

    update.sh update

--- update.sh
+++ update.sh
@@ -1,6 +1,16 @@
 sudo make uninstall
 sudo make clean install
 # remove all the stuff we don't need
+
+read -p "do you want to delete the files? s/N :" sn 
+
+sn=${sn,,}
+
+if [ "$sn" != "s" ]; then
+  exit 0
+fi
+
+
 rm -rf st 
 rm -rf config.h
 rm -f *.o 
