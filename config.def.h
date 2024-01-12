da48f7ad3a0b89bb4dae2805fbee48c68f5de380
Nefasque <Pedro0fici4lDev@gmail.com>
2024-01-12 14:27:49 -0400

    config beatufull

--- config.def.h
+++ config.def.h
@@ -14,23 +14,19 @@ github.com/Nefasque/st
 
 /* See LICENSE file for copyright and license details. */
 
-/*
- * appearance
+/* appearance
  *
- * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
- */
+ * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html */
 static char *font = "Fira Code Nerd Font:pixelsize=18:antialias=true:autohint=true";
-static int borderpx = 5;
+static int borderpx = 10;
 
-/*
- * What program is execed by st depends of these precedence rules:
+/* What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
  * 2: scroll and/or utmp
  * 3: SHELL environment variable
  * 4: value of shell in /etc/passwd
- * 5: value of shell in config.h
- */
-static char *shell = "/bin/sh";
+ * 5: value of shell in config.h */
+static char *shell = "/bin/bash";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
@@ -43,11 +39,9 @@ char *vtiden = "\033[?6c";
 static float cwscale = 1.0;
 static float chscale = 1.0;
 
-/*
- * word delimiter string
+/* word delimiter string
  *
- * More advanced example: L" `'\"()[]{}"
- */
+ * More advanced example: L" `'\"()[]{}" */
 wchar_t *worddelimiters = L" `'\"()[]{}";
 
 /* selection timeouts (in milliseconds) */
@@ -61,49 +55,38 @@ int allowaltscreen = 1;
    setting the clipboard text */
 int allowwindowops = 0;
 
-/*
- * draw latency range in ms - from new content/keypress/etc until drawing.
+/* draw latency range in ms - from new content/keypress/etc until drawing.
  * within this range, st draws when content stops arriving (idle). mostly it's
  * near minlatency, but it waits longer for slow updates to avoid partial draw.
- * low minlatency will tear/flicker more, as it can "detect" idle too early.
- */
+ * low minlatency will tear/flicker more, as it can "detect" idle too early. */
 static double minlatency = 8;
 static double maxlatency = 33;
 
-/*
- * blinking timeout (set to 0 to disable blinking) for the terminal blinking
- * attribute.
- */
+/* blinking timeout (set to 0 to disable blinking) for the terminal blinking
+ * attribute. */
 static unsigned int blinktimeout = 800;
 
-/*
- * thickness of underline and bar cursors
- */
+/* thickness of underline and bar cursors */
 static unsigned int cursorthickness = 2;
 
-/*
- * 1: render most of the lines/blocks characters without using the font for
+/* 1: render most of the lines/blocks characters without using the font for
  *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
  *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
- * 0: disable (render all U25XX glyphs normally from the font).
- */
+ * 0: disable (render all U25XX glyphs normally from the font). */
 const int boxdraw = 1;
 const int boxdraw_bold = 1;
 
 /* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
 const int boxdraw_braille = 0;
 
-/*
- * bell volume. It must be a value between -100 and 100. Use 0 for disabling
- * it
- */
-static int bellvolume = 0;
+/* bell volume. It must be a value between -100 and 100. Use 0 for disabling
+ * it */
+static int bellvolume = 1;
 
 /* default TERM value */
 char *termname = "xterm-256color";
 
-/*
- * spaces per tab
+/* spaces per tab
  *
  * When you are changing this value, don't forget to adapt the »it« value in
  * the st.info and appropriately install the st.info in the environment where
@@ -115,9 +98,8 @@ char *termname = "xterm-256color";
  * -a` »tab0« should appear. You can tell the terminal to not expand tabs by
  *  running following command:
  *
- *	stty tabs
- */
-unsigned int tabspaces = 8;
+ *	stty tabs */
+unsigned int tabspaces = 2;
 
 /* bg opacity */
 float alpha = 0.8;
@@ -154,10 +136,8 @@ static const char *colorname[] = {
 };
 
 
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
+/* Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor */
 unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
 unsigned int defaultcs = 7;
@@ -170,45 +150,33 @@ unsigned int selectionfg = 7;
 /* Else if 1 keep original foreground-color of each cell => more colors :) */
 static int ignoreselfg = 1;
 
-/*
- * Default shape of cursor
+/* Default shape of cursor
  * 2: Block ("█")
  * 4: Underline ("_")
  * 6: Bar ("|")
- * 7: Snowman ("☃")
- */
+ * 7: Snowman ("☃") */
 static unsigned int cursorshape = 2;
 
-/*
- * Default columns and rows numbers
- */
-
+/* Default columns and rows numbers */
 static unsigned int cols = 80;
 static unsigned int rows = 24;
 
-/*
- * Default colour and shape of the mouse cursor
- */
+/* Default colour and shape of the mouse cursor */
 static unsigned int mouseshape = XC_xterm;
 static unsigned int mousefg = 7;
 static unsigned int mousebg = 0;
 
-/*
- * Color used to display font attributes when fontconfig selected a font which
- * doesn't match the ones requested.
- */
+/* Color used to display font attributes when fontconfig selected a font which
+ * doesn't match the ones requested. */
+
 static unsigned int defaultattr = 11;
 
-/*
- * Force mouse select/shortcuts while mask is active (when MODE_MOUSE is set).
+/* Force mouse select/shortcuts while mask is active (when MODE_MOUSE is set).
  * Note that if you want to use ShiftMask with selmasks, set this to an other
- * modifier, set to 0 to not use it.
- */
+ * modifier, set to 0 to not use it. */
 static uint forcemousemod = ShiftMask;
 
-/*
- * Xresources preferences to load at startup
- */
+/* Xresources preferences to load at startup */
 ResourcePref resources[] = {
 		{ "font",         STRING,  &font }, 
 		{ "color0",       STRING,  &colorname[0] },
@@ -243,10 +211,8 @@ ResourcePref resources[] = {
 		{ "alpha",        FLOAT,   &alpha },
 };
 
-/*
- * Internal mouse shortcuts.
- * Beware that overloading Button1 will disable the selection.
- */
+/* Internal mouse shortcuts.
+ * Beware that overloading Button1 will disable the selection. */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
@@ -277,8 +243,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
-/*
- * Special keys (change & recompile st.info accordingly)
+/* Special keys (change & recompile st.info accordingly)
  *
  * Mask value:
  * * Use XK_ANY_MOD to match the key no matter modifiers state
@@ -295,25 +260,18 @@ static Shortcut shortcuts[] = {
  *
  * Be careful with the order of the definitions because st searches in
  * this table sequentially, so any XK_ANY_MOD must be in the last
- * position for a key.
- */
+ * position for a key. */
 
-/*
- * If you want keys other than the X11 function keys (0xFD00 - 0xFFFF)
- * to be mapped below, add them to this array.
- */
+/* If you want keys other than the X11 function keys (0xFD00 - 0xFFFF)
+ * to be mapped below, add them to this array. */
 static KeySym mappedkeys[] = { -1 };
 
-/*
- * State bits to ignore when matching key or button events.  By default,
- * numlock (Mod2Mask) and keyboard layout (XK_SWITCH_MOD) are ignored.
- */
+/* State bits to ignore when matching key or button events.  By default,
+ * numlock (Mod2Mask) and keyboard layout (XK_SWITCH_MOD) are ignored. */
 static uint ignoremod = Mod2Mask|XK_SWITCH_MOD;
 
-/*
- * This is the huge key array which defines all compatibility to the Linux
- * world. Please decide about changes wisely.
- */
+/* This is the huge key array which defines all compatibility to the Linux
+ * world. Please decide about changes wisely. */
 static Key key[] = {
 	/* keysym           mask            string      appkey appcursor */
 	{ XK_KP_Home,       ShiftMask,      "\033[2J",       0,   -1},
@@ -538,17 +496,14 @@ static uint selmasks[] = {
 	[SEL_RECTANGULAR] = Mod1Mask,
 };
 
-/*
- * Printable characters in ASCII, used to estimate the advance width
- * of single wide characters.
- */
+/* Printable characters in ASCII, used to estimate the advance width
+ * of single wide characters. */
 static char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
 
-/**
- * Undercurl style. Set UNDERCURL_STYLE to one of the available styles.
+/* Undercurl style. Set UNDERCURL_STYLE to one of the available styles.
  *
  * Curly: Dunno how to draw it *shrug*
  *  _   _   _   _
@@ -569,4 +524,4 @@ static char ascii_printable[] =
 #define UNDERCURL_SPIKY 1
 #define UNDERCURL_CAPPED 2
 // Active style
-#define UNDERCURL_STYLE UNDERCURL_SPIKY
+#define UNDERCURL_STYLE UNDERCURL_CURLY
