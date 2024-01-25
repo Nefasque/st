dce297c272ecef5c901a6b2dbc11acb2298d6bd2
Nefasque <Pedro0fici4lDev@gmail.com>
2024-01-25 15:43:10 -0400

    color selection chagen

--- config.def.h
+++ config.def.h
@@ -144,11 +144,11 @@ unsigned int defaultcs = 7;
 static unsigned int defaultrcs = 256;
 
 /* Colors used for selection */
-unsigned int selectionbg = 257;
-unsigned int selectionfg = 7;
+unsigned int selectionbg = 3;
+unsigned int selectionfg = 00;
 /* If 0 use selectionfg as foreground in order to have a uniform foreground-color */
 /* Else if 1 keep original foreground-color of each cell => more colors :) */
-static int ignoreselfg = 1;
+static int ignoreselfg = 0;
 
 /* Default shape of cursor
  * 2: Block ("█")
