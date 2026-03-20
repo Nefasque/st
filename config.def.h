42e5a03fa15252831bbfa5e3fe326a7467419d1c
Nefasque <nefasquetsu@gmail.com>
2026-03-20 16:08:42 +0000

    backgroup color update

--- config.def.h
+++ config.def.h
@@ -196,7 +196,7 @@ ResourcePref resources[] = {
 		{ "color13",      STRING,  &colorname[13] },
 		{ "color14",      STRING,  &colorname[14] },
 		{ "color15",      STRING,  &colorname[15] },
-		{ "color16",	  STRING,  &colorname[256] },
+		{ "color16",	  	STRING,  &colorname[256] },
 		{ "foreground",   STRING,  &colorname[257] },
 		{ "cursorColor",  STRING,  &colorname[258] },
 		{ "termname",     STRING,  &termname },
